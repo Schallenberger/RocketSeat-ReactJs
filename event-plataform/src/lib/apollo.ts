fa518195627a26ff6d19190d2507222e330e4d0e
Bruno Schallenberger <78933472+Schallenberger@users.noreply.github.com>
2022-06-27 15:12:19 -0300

    Update apollo.ts

--- event-plataform/src/lib/apollo.ts
+++ event-plataform/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri:'https://api-sa-east-1.graphcms.com/v2/cl4our3tp17rs01xk0g99hu4b/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCES_TOKEN}`
+  },
   cache: new InMemoryCache()
 });
